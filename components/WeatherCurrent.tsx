0b2589eb0f1551f0638462111be07d608701d82b
Niek Goedemans <99808100+N-Goedemans@users.noreply.github.com>
2023-11-28 18:17:33 +0100

    forecast updates

--- components/WeatherCurrent.tsx
+++ components/WeatherCurrent.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaTemperatureThreeQuarters } from "react-icons/fa6";
+import { ImCompass } from "react-icons/im";
+import { LuWind } from "react-icons/lu";
+import { FaRegEye } from "react-icons/fa";
+import { IoSunny } from "react-icons/io5";
+import { WiHumidity } from "react-icons/wi";
+import { FiSunrise } from "react-icons/fi";
+import { FiSunset } from "react-icons/fi";
 
 export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
   if (WeatherData) {
@@ -8,42 +15,67 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
   }
   return (
     <>
-      <div className="flex flex-col p-10">
-        <div className="flex justify-between">
-          <div className="flex flex-col justify-center">
-            <div className="flex flex-row items-center justify-between">
-              <div className="">
-                <h3 className="text-3xl">Today</h3>
-                <h1>date today</h1>
+      <div className="flex flex-col p-10 px-40">
+        <div className="flex justify-evenly">
+          <div className="flex justify-between">
+            <div className="flex flex-col justify-center pt-16">
+              <div className="flex flex-row items-center justify-between">
+                <div>
+                  <h3 className="text-3xl">Today</h3>
+                  <h1>date today</h1>
+                </div>
+                <div>
+                  <img
+                    src={WeatherData?.current?.condition?.icon}
+                    width={50}
+                    height={50}
+                    alt="Weather Icon"
+                  />
+                </div>
               </div>
-              <div>
-                <img
-                  src={WeatherData?.current?.condition?.icon}
-                  width={50}
-                  height={50}
-                  alt="Weather Icon"
-                />
+              <div></div>
+              <div className="text-3xl">
+                <h5>{WeatherData?.current?.temp_c}°C</h5>
               </div>
-            </div>
-            <div>
-              
-            </div>
-            <div className="text-3xl">
-              <h5>{WeatherData?.current?.temp_c}°C</h5>
-            </div>
-            <div className="text-black bg-white rounded-xl p-2 text-sm flex items-center">
-              <div className="pr-2">
-                <FaLocationDot />
-              </div>
-              <div>
-                {WeatherData?.location?.region
-                  ? `${WeatherData?.location?.name}, ${WeatherData?.location?.region}`
-                  : WeatherData?.location?.name}
+              <div className="text-black bg-white rounded-xl p-2 text-sm flex items-center">
+                <div className="pr-2">
+                  <FaLocationDot />
+                </div>
+                <div>
+                  {WeatherData?.location?.region
+                    ? `${WeatherData?.location?.name}, ${WeatherData?.location?.region}`
+                    : WeatherData?.location?.name}
+                </div>
               </div>
             </div>
           </div>
+          <div>
+            <div className="grid grid-cols-3 gap-4">
+              {WeatherData?.forecast?.forecastday.map((day: any) => (
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-col justify-center pt-16">
+                    <div className="flex flex-row items-center justify-between">
+                      <div className="bg-white/40 rounded-lg p-4 items-center">
+                        <div>
+                          <img
+                            src={day?.day?.condition?.icon}
+                            height={50}
+                            width={50}
+                            alt="Weather Icon"
+                          />
+                        </div>
+                        <div>
+                          <h3 className="text-2xl">{day?.day?.avgtemp_c}°C</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="pt-72">
+        <div className="pt-52">
           <div className="pb-4 text-xl font-semibold">
             <h2>Weather Details</h2>
           </div>
@@ -51,13 +83,13 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div className="">
-                  <h3>Feels Like</h3>
+                  <h3 className="font-semibold">Feels Like</h3>
                   <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
+                    {WeatherData?.current?.feelslike_c}°C
                   </h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <FaTemperatureThreeQuarters size={30} />
                 </div>
               </div>
             </div>
@@ -65,13 +97,13 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div>
-                  <h3>Wind Speed</h3>
+                  <h3 className="font-semibold">Wind Direction</h3>
                   <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
+                    {WeatherData?.current?.wind_dir}
                   </h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <ImCompass size={30} />
                 </div>
               </div>
             </div>
@@ -79,13 +111,13 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div>
-                  <h3>Wind Speed</h3>
+                  <h3 className="font-semibold">Wind Speed</h3>
                   <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
+                    {WeatherData?.current?.wind_kph} km/h
                   </h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <LuWind size={30} />
                 </div>
               </div>
             </div>
@@ -93,13 +125,13 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div>
-                  <h3>Wind Speed</h3>
+                  <h3 className="font-semibold">Visibility</h3>
                   <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
+                    {WeatherData?.current?.vis_km} km
                   </h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <FaRegEye size={30} />
                 </div>
               </div>
             </div>
@@ -107,13 +139,11 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div>
-                  <h3>Wind Speed</h3>
-                  <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
-                  </h3>
+                  <h3 className="font-semibold">UV Radiation</h3>
+                  <h3 className="text-left">{WeatherData?.current?.uv}</h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <IoSunny size={30} />
                 </div>
               </div>
             </div>
@@ -121,13 +151,13 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div>
-                  <h3>Wind Speed</h3>
+                  <h3 className="font-semibold">Humidity</h3>
                   <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
+                    {WeatherData?.current?.humidity}
                   </h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <WiHumidity size={30} />
                 </div>
               </div>
             </div>
@@ -135,13 +165,13 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div>
-                  <h3>Wind Speed</h3>
+                  <h3 className="font-semibold">Sunrise</h3>
                   <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
+                    {WeatherData?.forecast?.forecastday[0]?.astro?.sunrise}
                   </h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <FiSunrise size={30} />
                 </div>
               </div>
             </div>
@@ -149,13 +179,13 @@ export default function WeatherCurrent({ WeatherData }: { WeatherData: any }) {
             <div className=" bg-white/70 p-4 rounded-xl">
               <div className="flex items-center justify-evenly">
                 <div>
-                  <h3>Wind Speed</h3>
+                  <h3 className="font-semibold">Sunset</h3>
                   <h3 className="text-left">
-                    {WeatherData?.current?.feelslike_c}
+                    {WeatherData?.forecast?.forecastday[0]?.astro?.sunset}
                   </h3>
                 </div>
                 <div>
-                  <FaTemperatureThreeQuarters size={20} />
+                  <FiSunset size={30} />
                 </div>
               </div>
             </div>
